afe69619312e4c8b4ce50672ef38d426a0a61974
lijingen <17732235526@163.com>
2023-06-16 15:12:23 +0800

    设备状态页面初步

---
+++ scada-core-ui/src/views/scada/partstatus/components/drivelist.vue
@@ -0,0 +1,217 @@
+<template>
+  <div class="card" style="background-color: $bg-elevation-3; height: 700px">
+    <div class="card-body" style="color: #9fdfdf">
+      <CRow>
+        <CCol>
+          <CListGroup flush>
+            <CListGroupItem>齿箱润滑油位保护</CListGroupItem>
+            <CListGroupItem>齿箱润滑油压保护</CListGroupItem>
+            <CListGroupItem>主轴齿箱侧温度</CListGroupItem>
+            <CListGroupItem>主轴叶轮侧温度</CListGroupItem>
+            <CListGroupItem>驱动器</CListGroupItem>
+            <CListGroupItem>95度限位开关</CListGroupItem>
+            <CListGroupItem>制维护模式开关</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+            <CListGroupItem>90度位置传感器</CListGroupItem>
+            <CListGroupItem>心跳信号</CListGroupItem>
+            <CListGroupItem>润滑反馈1</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+          </CListGroup>
+        </CCol>
+        <CCol>
+          <CListGroup style="background-color: #011240; text-align: center">
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color:brown">
+              压力异常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+          </CListGroup>
+        </CCol>
+        <CCol>
+          <CListGroup flush>
+            <CListGroupItem>叶轮转速1</CListGroupItem>
+            <CListGroupItem>叶轮转速2</CListGroupItem>
+            <CListGroupItem>主轴齿箱侧温度</CListGroupItem>
+            <CListGroupItem>主轴叶轮侧温度</CListGroupItem>
+            <CListGroupItem>驱动器</CListGroupItem>
+            <CListGroupItem>95度限位开关</CListGroupItem>
+            <CListGroupItem>制维护模式开关</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+            <CListGroupItem>90度位置传感器</CListGroupItem>
+            <CListGroupItem>心跳信号</CListGroupItem>
+            <CListGroupItem>润滑反馈1</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+          </CListGroup>
+        </CCol>
+        <CCol>
+          <CListGroup style="background-color: #011240; text-align: center">
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+          </CListGroup>
+        </CCol>
+        <CCol>
+          <CListGroup flush>
+            <CListGroupItem>叶轮转速1</CListGroupItem>
+            <CListGroupItem>叶轮转速2</CListGroupItem>
+            <CListGroupItem>主轴齿箱侧温度</CListGroupItem>
+            <CListGroupItem>主轴叶轮侧温度</CListGroupItem>
+            <CListGroupItem>驱动器</CListGroupItem>
+            <CListGroupItem>95度限位开关</CListGroupItem>
+            <CListGroupItem>制维护模式开关</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+            <CListGroupItem>90度位置传感器</CListGroupItem>
+            <CListGroupItem>心跳信号</CListGroupItem>
+            <CListGroupItem>润滑反馈1</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+          </CListGroup>
+        </CCol>
+        <CCol>
+          <CListGroup style="background-color: #011240; text-align: center">
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+            <CListGroupItem style="color: aquamarine">
+              油温正常
+            </CListGroupItem>
+          </CListGroup>
+        </CCol>
+      </CRow>
+    </div>
+  </div>
+</template>
+          
+    <script>
+export default {
+  name: "DriveList",
+  data() {
+    return {};
+  },
+  props: {
+    fanName: String, //风机名称
+    fanStatus: String, //风机状态
+    alertLevel: String, //警报等级
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+       
--- scada-core-ui/src/views/scada/partstatus/components/wheellist.vue
+++ scada-core-ui/src/views/scada/partstatus/components/wheellist.vue
@@ -1,15 +1,367 @@
 <template>
-  <div class="card" style="background: #011240; height: 700px">
+  <div class="card" style="background-color: $bg-elevation-3; height: 700px">
     <div class="card-body" style="color: #9fdfdf">
       <CRow>
-        <CCol sm="4" lg="4">
-          <CRow>
-            <CCol sm="4" lg="4">叶轮转速1 </CCol>
-            <CCol sm="4" lg="4">0.2rad/s | - | - </CCol>
-          </CRow>
+        <CCol> 
+          <CListGroup flush>
+            <CListGroupItem>叶轮转速1</CListGroupItem>
+            <CListGroupItem>叶轮转速2</CListGroupItem>
+            <CListGroupItem>主轴齿箱侧温度</CListGroupItem>
+            <CListGroupItem>主轴叶轮侧温度</CListGroupItem>
+            <CListGroupItem>驱动器</CListGroupItem>
+            <CListGroupItem>95度限位开关</CListGroupItem>
+            <CListGroupItem>制维护模式开关</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+            <CListGroupItem>90度位置传感器</CListGroupItem>
+            <CListGroupItem>心跳信号</CListGroupItem>
+            <CListGroupItem>润滑反馈1</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+          </CListGroup>
         </CCol>
-        <CCol sm="4" lg="4"> </CCol>
-        <CCol sm="4" lg="4"> </CCol>
+        <CCol>
+          <CListGroup style="background-color: #011240;text-align: center;">
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 0.2rad/s </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 0.2rad/s </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 36.7℃ </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 26.7℃ </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 1 </CCol>
+                <CCol class="link-right"> 0 </CCol>
+                <CCol> 0 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+          </CListGroup>
+        </CCol>
+        <CCol> 
+          <CListGroup flush>
+            <CListGroupItem>叶轮转速1</CListGroupItem>
+            <CListGroupItem>叶轮转速2</CListGroupItem>
+            <CListGroupItem>主轴齿箱侧温度</CListGroupItem>
+            <CListGroupItem>主轴叶轮侧温度</CListGroupItem>
+            <CListGroupItem>驱动器</CListGroupItem>
+            <CListGroupItem>95度限位开关</CListGroupItem>
+            <CListGroupItem>制维护模式开关</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+            <CListGroupItem>90度位置传感器</CListGroupItem>
+            <CListGroupItem>心跳信号</CListGroupItem>
+            <CListGroupItem>润滑反馈1</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+          </CListGroup>
+        </CCol>
+        <CCol> 
+          <CListGroup style="background-color: #011240;text-align: center;">
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 0.2rad/s </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 0.2rad/s </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 36.7℃ </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 26.7℃ </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 1 </CCol>
+                <CCol class="link-right"> 0 </CCol>
+                <CCol> 0 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+          </CListGroup>
+        </CCol>
+        <CCol> 
+          <CListGroup flush>
+            <CListGroupItem>叶轮转速1</CListGroupItem>
+            <CListGroupItem>叶轮转速2</CListGroupItem>
+            <CListGroupItem>主轴齿箱侧温度</CListGroupItem>
+            <CListGroupItem>主轴叶轮侧温度</CListGroupItem>
+            <CListGroupItem>驱动器</CListGroupItem>
+            <CListGroupItem>95度限位开关</CListGroupItem>
+            <CListGroupItem>制维护模式开关</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+            <CListGroupItem>90度位置传感器</CListGroupItem>
+            <CListGroupItem>心跳信号</CListGroupItem>
+            <CListGroupItem>润滑反馈1</CListGroupItem>
+            <CListGroupItem>强维护模式开关</CListGroupItem>
+            <CListGroupItem>SSI编码器</CListGroupItem>
+          </CListGroup>
+        </CCol>
+        <CCol> 
+          <CListGroup style="background-color: #011240;text-align: center;">
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 0.2rad/s </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 0.2rad/s </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 36.7℃ </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 26.7℃ </CCol>
+                <CCol class="link-right"> ---- </CCol>
+                <CCol> ---- </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 1 </CCol>
+                <CCol class="link-right"> 0 </CCol>
+                <CCol> 0 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+            <CListGroupItem style="color:aquamarine">
+              <CRow>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol class="link-right"> 正常 </CCol>
+                <CCol> 正常 </CCol>
+              </CRow>
+            </CListGroupItem>
+          </CListGroup>
+        </CCol>
       </CRow>
     </div>
   </div>
